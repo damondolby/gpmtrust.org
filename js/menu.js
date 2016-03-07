9b76fd03c370a48ce4306b4e3f7df21298ad4504
Damon <damondolby@gmail.com>
2016-03-07 23:16:55 +0000

    initial commit

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+document.write('<div id="left">');
+document.write('	<a class="menulink" href="default.html">home</a>');
+document.write('	<a class="menulink" href="history.html">history</a>');
+document.write('	<a class="menulink" href="letters.html">letters</a>');
+document.write('	<a class="menulink" href="links.html">links</a>');
+document.write('	<a class="menulink" href="criteria.html">criteria</a>');
+document.write('</div>');										
