bba74634fe90dbceecd52dfb2e20de58b15733d0
Damon <damondolby@gmail.com>
2018-09-13 23:36:57 +0100

    cookie policy / privacy policy changes

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+document.write('<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="7ae7d41b-3ccb-407d-a489-5bf90b0ab42c" type="text/javascript" async></script>');
 document.write('<div id="left">');
 document.write('	<a class="menulink" href="default.html">home</a>');
 document.write('	<a class="menulink" href="history.html">history</a>');
